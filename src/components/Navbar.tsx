1ee8ba78e8f6c58536bfa90a5946925a20baee76
ChimpanCode <ranc.music@gmail.com>
2025-06-07 01:05:16 +0900

    ナビゲーションバーで、現在選択している機能を強調表示するように変更

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 
 const Navbar = () => {
+  const location = useLocation(); // 現在のパスを取得
+
+   // ナビリンクの情報
+  const navLinks = [
+    { to: "/", label: "ホーム" },
+    { to: "/quiz", label: "機能1（用語クイズ）" },
+    { to: "/verbalize", label: "機能2（用語の言語化）" },
+    { to: "/studyLog", label: "機能3（学習管理）" },
+  ];
+
+
   return (
     <nav>
       <span className="app-title">
         Road to AP
       </span>
       <div className="nav-links">
-        <Link to="/">ホーム</Link>
-        <Link to="/quiz">機能1（用語クイズ）</Link>
-        <Link to="/verbalize">機能2（用語の言語化）</Link>
-        <Link to="/studyLog">機能3（学習管理）</Link>
+        {navLinks.map(link => (
+          <Link
+            key={link.to}
+            to={link.to}
+            className={location.pathname === link.to ? "active" : ""}
+          >
+            {link.label}
+          </Link>
+        ))}
       </div>
       <span className="">
         <Link to="/login">ログイン</Link>
